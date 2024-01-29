ebc40eed1c9a7b45b62f70196fc5c96c0f2442aa
islamshahil <shahilislam@gmail.com>
2024-01-30 03:13:33 +0530

    deployment to prod

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ def handle_connect():
     print('Client connected')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, use_reloader=False)
